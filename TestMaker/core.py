fd88aeb15ec23a838f28c476080413e6c42410f5
Ian Dennis Miller <ian@iandennismiller.com>
2012-12-02 12:06:15 -0500

    splitting out Version class entirely

--- TestMaker/core.py
+++ TestMaker/core.py
@@ -1,85 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import csv, random, os, string, re, sys, json
-import jinja2
 import codecs
 from optparse import OptionParser
+from Version import TestVersion
 
 def UnicodeDictReader(utf8_data, **kwargs):
     csv_reader = csv.DictReader(utf8_data, **kwargs)
     for row in csv_reader:
         yield dict([(key, unicode(value, 'utf-8')) for key, value in row.iteritems()])
 
-class Version(object):
-    def __init__(self, testmaker, version_name, key=False):
-        self.version = version_name
-        self.tm = testmaker
-        self.key = key
-
-        self.mapping = self.get_mapping()
-
-    def get_mapping(self):
-        filename = os.path.join(self.tm.cfg["version_path"], "%s.json" % self.version)
-        filename = self.tm.get_filename(filename)
-        with open(filename, "r") as f:
-            mapping = json.load(f)
-        return mapping
-
-    def render_question(self, question, mapping):
-        template = self.tm.jinja.get_template(self.tm.cfg["templates"]["question"])
-        choices = [
-            u"\CorrectChoice {0}".format(question['correct']),
-            u"\choice {0}".format(question['foil1']),
-            u"\choice {0}".format(question['foil2']),
-            u"\choice {0}".format(question['foil3']),
-        ]
-        
-        # place the choices in the order specified by the mapping
-        arranged = [
-            choices[mapping[0]],
-            choices[mapping[1]],
-            choices[mapping[2]],
-            choices[mapping[3]],
-        ]
-
-        source = '\\textbf{(Source: %s)}' % question['source']
-        # here replace ___ with TeX underlines
-        question_str = re.sub(r'_+', "\underline{\hspace*{0.5in}}", question['question'])
-        arranged = u"\t\t" + u"\n\t\t".join(arranged)
-        return template.render(choices=arranged, question=question_str, source=source)
-
-    def render(self):
-        questions = u""
-        for (original, mapping) in self.mapping:
-            question = self.tm.questions[original]
-            questions += self.render_question(question, mapping)
-
-        template = self.tm.jinja.get_template('exam.tex')
-        assets_latex = '\graphicspath{{%s/}}' % self.tm.get_filename(self.tm.cfg["assets_path"])
-
-        if self.key:
-            outfile_tex = os.path.join(self.tm.cfg["output_path"], "%s KEY.tex" % self.version)
-        else:
-            outfile_tex = os.path.join(self.tm.cfg["output_path"], "%s.tex" % self.version)
-        outfile_tex = self.tm.get_filename(outfile_tex)
-        with codecs.open(outfile_tex, 'w', encoding="utf-8") as texfile:
-            if self.key:
-                rendered = template.render(questions=questions, version="%s KEY" % self.version, answers="answers,", assets_path=assets_latex)
-            else:
-                rendered = template.render(questions=questions, version=self.version, answers="", assets_path=assets_latex)
-            texfile.write(rendered)
-
-        cmd = "pdflatex -output-directory=%s '%s'" % \
-            (self.tm.get_filename(self.tm.cfg["output_path"]), outfile_tex)
-        os.system(cmd)
-
 class TestMaker(object):
     def __init__(self, filename):
         with open(filename) as f:
             self.cfg = json.load(f)
         self.questions = list()
-        loader = jinja2.FileSystemLoader(self.get_filename(self.cfg["template_path"]))
-        self.jinja = jinja2.Environment(loader=loader)
 
     def get_filename(self, bare_filename):
         if "root_path" in self.cfg:
